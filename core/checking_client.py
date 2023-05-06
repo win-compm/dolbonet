b3e3daa868c8d87c4d70320a986c630a658865da
mw_dev <79410241+win-compm@users.noreply.github.com>
2023-05-06 12:10:46 +0300

    Add files via upload

---
+++ core/checking_client.py
@@ -0,0 +1,13 @@
+# Тестовый Discord клиент для проверки валидности Discord токена
+# by Sergree
+# https://github.com/sergree
+
+import discord
+
+
+class CheckingClient(discord.Client):
+    def __init__(self, **options):
+        super().__init__(**options)
+
+    async def on_ready(self):
+        await self.close()
